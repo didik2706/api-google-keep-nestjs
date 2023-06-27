cf74f8486e55d03c6e45ab2aa4111e3a983c00ff
didik2706 <didikhidayat27@gmail.com>
2023-06-27 13:24:25 +0700

    activate logger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
 
   // setup versioning
   app.enableVersioning({
